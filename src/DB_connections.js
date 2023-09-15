1d56ba5592d0a84509136668e3ee7bb9638335ae
German <german2717@gmail.com>
2023-09-15 19:01:14 -0300

    ger

--- src/DB_connections.js
+++ src/DB_connections.js
@@ -2,8 +2,6 @@ require("dotenv").config();
 require("pg");
 const { Sequelize } = require("sequelize");
 const { DB_DEPLOY } = process.env;
-const FavoriteModel = require("./models/Favorite");
-const UserModel = require("./models/User");
 
 const sequelize = new Sequelize(DB_DEPLOY, {
   logging: false,
@@ -19,15 +17,16 @@ const sequelize = new Sequelize(DB_DEPLOY, {
 //   }
 // );
 
-FavoriteModel(sequelize);
-UserModel(sequelize);
+// FavoriteModel(sequelize);
+// UserModel(sequelize);
 //
 const { User, Favorite } = sequelize.models;
 User.belongsToMany(Favorite, { through: "user_favorite" });
 Favorite.belongsToMany(User, { through: "user_favorite" });
 
+const FavoriteModel = require("./models/Favorite")(sequelize, Sequelize);
+const UserModel = require("./models/User")(sequelize, Sequelize);
+
 module.exports = {
-  User,
-  Favorite,
-  conn: sequelize,
+  sequelize,
 };
